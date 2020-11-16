b2b522d4e270c73a2af8845d1724d91dfd2e49de
Miek Gieben <miek@miek.nl>
2020-11-16 16:56:27 +0100

    Fix tetss
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- systemd/pod_test.go
+++ systemd/pod_test.go
@@ -3,7 +3,7 @@ package systemd
 import "testing"
 
 func TestUidFromUnitName(t *testing.T) {
-	name := ImageNameToUnitNameUID("default", "openssh-server", "we4rw4")
+	name := PodToUnitName("default", "openssh-server", "openssh-server", "we4rw4")
 	uid := UidFromUnitName(name)
 	if uid != "we4rw4" {
 		t.Errorf("expected %s, got %s", "we4rw4", uid)
