77211f0a148996439a6e9a6171ae1e56ca28bc9b
Miek Gieben <miek@miek.nl>
2020-12-01 16:38:27 +0100

    Updates
    * update yaml files to just use 'bash', we use PATH to make it absolute
      anyway
    * log that policyrcd-script-zg2 was already installed
    
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- systemd/provider.go
+++ systemd/provider.go
@@ -45,8 +45,12 @@ func New(cfg provider.InitConfig) (*P, error) {
 
 		// Just installed pre-requisites instead of pointing to the docs.
 		log.Printf("Installing %s, to prevent installed daemons from starting", "policyrcd-script-zg2")
-		if err, _ := p.pkg.Install("policyrcd-script-zg2", ""); err != nil {
+		err, ok := p.pkg.Install("policyrcd-script-zg2", "")
+		if err != nil {
 			log.Printf("Failed to install %s, %s. Continuing anyway", "policyrcd-script-zg2", err)
+		}
+		if ok {
+			log.Printf("%s is already installed", "policyrcd-script-zg2")
 		}
 
 	case "arch":
