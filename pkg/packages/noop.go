0a804f7bcf423ee9804cf55e1d03d57ba47dbf87
Pires <pjpires@gmail.com>
2020-12-12 13:22:13 +0000

    Introduce a no-op package manager
    Signed-off-by: Pires <pjpires@gmail.com>

---
+++ pkg/packages/noop.go
@@ -0,0 +1,27 @@
+package packages
+
+import (
+	"fmt"
+)
+
+// NoopPackageManager implements a no-op of the PackageManager interface.
+// Its purpose is to enable scenarios where no package handling is required,
+// i.e. the necessary executables are already available on the host.
+type NoopPackageManager struct{}
+
+func (p *NoopPackageManager) Setup() error {
+	return nil
+}
+
+func (p *NoopPackageManager) Install(pkg, version string) (error, bool) {
+	return nil, true
+}
+
+func (p *NoopPackageManager) Unitfile(pkg string) (string, error) {
+	// This is fine as pod creation will synthesize a unit file.
+	return "", fmt.Errorf("noop")
+}
+
+func (p *NoopPackageManager) Clean(pkg string) string {
+	return pkg
+}
