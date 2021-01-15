57c31f0dad71ab413f0f039b955077a57c73df62
Pires <pjpires@gmail.com>
2021-01-15 14:50:56 +0000

    pod: fix unwatch
    Signed-off-by: Pires <pjpires@gmail.com>

--- systemd/watch_test.go
+++ systemd/watch_test.go
@@ -2,25 +2,59 @@ package systemd
 
 import (
 	"testing"
+
+	"k8s.io/apimachinery/pkg/types"
 )
 
-func TestWatchUnWatch(t *testing.T) {
+func TestWatcher(t *testing.T) {
 	w := newWatcher()
+
+	if len(w.configs) != 0 {
+		t.Fatal("expected no configMaps to be watched")
+	}
+	if len(w.cmKeysByPod) != 0 {
+		t.Fatal("expected no configMaps to be watched")
+	}
+
 	pod, err := podFromFile("testdata/uptimed-config.yaml")
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	w.Watch(pod)
+
 	if len(w.configs) != 1 {
 		t.Fatal("expected 1 configMap to be watched")
 	}
-	pods := w.configs["default.uptimed-conf"] // namespace . configMap name
-	if len(pods) != 1 {
-		t.Fatal("expected pod to be returned for watch")
+	var configMapKey types.NamespacedName
+	for k, _ := range w.configs {
+		configMapKey = k
 	}
 
-	w.Unwatch(pods[0]) // call unwatch with the returned pod, so we test the correct pod has been stored
+	pods, _ := w.configs[configMapKey]
+	if len(pods) != 1 {
+		t.Fatal("expected one pod to be known to the watcher")
+	}
+
+	if len(w.cmKeysByPod) != 1 {
+		t.Fatal("expected one pod to be known to the watcher")
+	}
+	podKey := types.NamespacedName{Namespace: pod.Namespace, Name: pod.Name}
+	if _, ok := w.cmKeysByPod[podKey]; !ok {
+		t.Fatalf("expected pod %q to be known to the watcher", podKey)
+	}
+
+	w.Unwatch(pod)
+
 	if len(w.configs) != 0 {
 		t.Fatal("expected no configMaps to be watched")
+	}
+	if len(w.cmKeysByPod) != 0 {
+		t.Fatal("expected no configMaps to be watched")
+	}
+
+	pods, _ = w.configs[configMapKey]
+	if len(pods) != 0 {
+		t.Fatal("expected no pods to be known to the watcher")
 	}
 }
